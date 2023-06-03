5a3282805125368ef717a95a843461d005e089aa
unknown <carlosvalente107@gmail.com>
2023-06-03 15:28:28 -0300

    first commit

---
+++ Clinica/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+// isso é um componente
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'serasa-tranformese';
+
+  // String nome = "Serasa";
+}
+
+
+// public class Cliente 
+// exports class ClienteComponet
